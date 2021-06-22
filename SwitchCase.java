8b96421e3f8bf11ba4ae4ef5bedb63460baed1dc
Vinay Reddy  Meka <reddy@Vinays-MBP.lan1>
2021-06-21 19:51:56 -0500

    Changed the values in switch case

--- SwitchCase.java
+++ SwitchCase.java
@@ -3,8 +3,8 @@ package eficens;
 public class SwitchCase {
 
 	public static void main(String[] args) {
-		double i = 10;
-		double j = 3;
+		double i = 100;
+		double j = 30;
 		char operation= '/';
 		double result;
 		
